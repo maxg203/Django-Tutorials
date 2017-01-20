be1565099e2cdac1d7279e187d1bc719ce6b3d7f
Max Goodridge <max@vidwork.io>
2017-01-20 23:32:56 +0000

    Updated admin interface and added a manager

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from accounts.models import UserProfile
 
-# Register your models here.
-admin.site.register(UserProfile)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_info', 'city', 'phone', 'website')
+
+    def user_info(self, obj):
+        return obj.description
+
+    def get_queryset(self, request):
+        queryset = super(UserProfileAdmin, self).get_queryset(request)
+        queryset = queryset.order_by('-phone', 'user')
+        return queryset
+
+    user_info.short_description = 'Info'
+
+admin.site.register(UserProfile, UserProfileAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
-# Create your models here.
+class UserProfileManager(models.Manager):
+    def get_queryset(self):
+        return super(UserProfileManager, self).get_queryset().filter(city='London')
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     description = models.CharField(max_length=100, default='')
@@ -10,6 +13,8 @@ class UserProfile(models.Model):
     website = models.URLField(default='')
     phone = models.IntegerField(default=0)
     image = models.ImageField(upload_to='profile_image', blank=True)
+
+    london = UserProfileManager()
 
     def __str__(self):
         return self.user.username
