0b6b4f8c3d2382cb5ebe5fce762e38e3a4f998fc
Max Goodridge <max@vidwork.io>
2017-02-06 22:58:42 +0000

    Fixed home page link, moved to class based view and added a basic home page template

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-from home import views
+from home.views import HomeView
 
 urlpatterns = [
-    url(r'^$', views.home, name='home')
+    url(r'^$', HomeView.as_view(), name='home')
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def home(request):
-    return HttpResponse('It works!')
+class HomeView(TemplateView):
+    template_name = 'home/home.html'
