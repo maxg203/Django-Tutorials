d880e6ec75f1ce7d4e1a2cc93103b2658139cd33
Max Goodridge <max.goodridge@maplecroft.com>
2017-02-16 21:26:43 +0000

    Added post support for Django form on the home page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from home.forms import HomeForm
 
@@ -10,3 +10,13 @@ class HomeView(TemplateView):
     def get(self, request):
         form = HomeForm()
         return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['post']
+            form = HomeForm()
+            return redirect('home:home')
+
+        args = {'form': form, 'text': text}
+        return render(request, self.template_name, args)
