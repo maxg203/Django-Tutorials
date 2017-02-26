3c858914ee2acadab6edd38b3107cc5d18160f8b
Max Goodridge <work@maxgoodridge.com>
2017-02-26 16:29:09 +0000

    Posts on the home page are now filtered by date

--- home/models.py
+++ home/models.py
@@ -5,4 +5,5 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     post = models.CharField(max_length=500)
     user = models.ForeignKey(User)
-    date = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
--- home/views.py
+++ home/views.py
@@ -10,7 +10,7 @@ class HomeView(TemplateView):
 
     def get(self, request):
         form = HomeForm()
-        posts = Post.objects.all()
+        posts = Post.objects.all().order_by('-created')
 
         args = {'form': form, 'posts': posts}
         return render(request, self.template_name, args)
