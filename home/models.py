38c8bb3edb247a2b10007d7317dea25104112a3d
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-03-27 23:53:40 +0100

    Added friend creation helper methods

--- home/models.py
+++ home/models.py
@@ -11,3 +11,18 @@ class Post(models.Model):
 
 class Friend(models.Model):
     users = models.ManyToManyField(User)
+    current_user = models.ForeignKey(User, related_name='owner', null=True)
+
+    @classmethod
+    def make_friend(cls, current_user, new_friend):
+        friend, created = cls.objects.get_or_create(
+            current_user=current_user
+        )
+        friend.users.add(new_friend)
+
+    @classmethod
+    def lose_friend(cls, current_user, new_friend):
+        friend, created = cls.objects.get_or_create(
+            current_user=current_user
+        )
+        friend.users.remove(new_friend)
