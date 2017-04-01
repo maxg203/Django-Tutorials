dfeb256f1038ef03011a9483a11455ad2a581039
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-04-01 17:58:39 +0100

    Added friends to the home page

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from home.views import HomeView
+from . import views
 
 urlpatterns = [
-    url(r'^$', HomeView.as_view(), name='home')
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^connect/(?P<operation>.+)/(?P<pk>\d+)/$', views.change_friends, name='change_friends')
 ]
--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 
 from home.forms import HomeForm
-from home.models import Post
+from home.models import Post, Friend
 
 
 class HomeView(TemplateView):
@@ -13,8 +13,12 @@ class HomeView(TemplateView):
         form = HomeForm()
         posts = Post.objects.all().order_by('-created')
         users = User.objects.exclude(id=request.user.id)
+        friend = Friend.objects.get(current_user=request.user)
+        friends = friend.users.all()
 
-        args = {'form': form, 'posts': posts, 'users': users}
+        args = {
+            'form': form, 'posts': posts, 'users': users, 'friends': friends
+        }
         return render(request, self.template_name, args)
 
     def post(self, request):
@@ -30,3 +34,11 @@ class HomeView(TemplateView):
 
         args = {'form': form, 'text': text}
         return render(request, self.template_name, args)
+
+def change_friends(request, operation, pk):
+    friend = User.objects.get(pk=pk)
+    if operation == 'add':
+        Friend.make_friend(request.user, friend)
+    elif operation == 'remove':
+        Friend.lose_friend(request.user, friend)
+    return redirect('home:home')
