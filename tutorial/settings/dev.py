a2bbbd911ab3036c7621c9dba88301a7940ad573
Max Goodridge <max.goodridge@hotmail.co.uk>
2017-04-21 17:37:18 +0100

    Updated settings

--- tutorial/settings/dev.py
+++ tutorial/settings/dev.py
@@ -2,7 +2,12 @@ from tutorial.settings.base import *
 
 # Override base.py settings here
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 try:
     from tutorial.settings.local import *
