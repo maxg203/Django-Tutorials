c29460be6ed2788b256ee17d18603dcf5e0022a2
ahmed_ibrahim <aibrahim66@yahoo.com>
2018-04-30 05:37:02 +0200

    fix syntax error

--- tutorial/middleware.py
+++ tutorial/middleware.py
@@ -26,9 +26,9 @@ class LoginRequiredMiddleware:
         if path == reverse('accounts:logout').lstrip('/'):
             logout(request)
 
-        if request.user.is_authenticated() and url_is_exempt:
+        if request.user.is_authenticated and url_is_exempt:
             return redirect(settings.LOGIN_REDIRECT_URL)
-        elif request.user.is_authenticated() or url_is_exempt:
+        elif request.user.is_authenticated or url_is_exempt:
             return None
         else:
             return redirect(settings.LOGIN_URL)
