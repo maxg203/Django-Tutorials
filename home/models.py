7875946455b60d09f7e48cb103ffc85a77293b97
ahmed_ibrahim <aibrahim66@yahoo.com>
2018-04-30 05:37:29 +0200

    add on_delete property

--- home/models.py
+++ home/models.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     post = models.CharField(max_length=500)
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User,on_delete=models.DO_NOTHING)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
 
 class Friend(models.Model):
     users = models.ManyToManyField(User)
-    current_user = models.ForeignKey(User, related_name='owner', null=True)
+    current_user = models.ForeignKey(User, related_name='owner', null=True,on_delete=models.DO_NOTHING)
 
     @classmethod
     def make_friend(cls, current_user, new_friend):
