4efd05fd6065b8e5fce17d7977fb3c8b123810c4
ahmed_ibrahim <aibrahim66@yahoo.com>
2018-05-04 02:55:23 +0200

    fix some bugs

--- home/views.py
+++ home/views.py
@@ -34,10 +34,10 @@ class HomeView(TemplateView):
         args = {'form': form, 'text': text}
         return render(request, self.template_name, args)
 
-    def change_friends(request, operation, pk):
-        friend = User.objects.get(pk=pk)
-        if operation == 'add':
-            Friend.make_friend(request.user, friend)
-        elif operation == 'remove':
-            Friend.lose_friend(request.user, friend)
-        return redirect('home:home')
+def change_friends(request, operation, pk):
+    friend = User.objects.get(pk=pk)
+    if operation == 'add':
+        Friend.make_friend(request.user, friend)
+    elif operation == 'remove':
+        Friend.lose_friend(request.user, friend)
+    return redirect('home:home')
