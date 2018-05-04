0855ffeb73246e07b37e8f5f91bb7fe7035e2cef
ahmed_ibrahim <aibrahim66@yahoo.com>
2018-05-04 02:50:34 +0200

    fix some bugs

--- home/views.py
+++ home/views.py
@@ -12,7 +12,6 @@ class HomeView(TemplateView):
         form = HomeForm()
         posts = Post.objects.all().order_by('-created')
         users = User.objects.exclude(id=request.user.id)
-        friends = []
         friend, created = Friend.objects.get_or_create(current_user=request.user)
         friends = friend.users.all()
 
